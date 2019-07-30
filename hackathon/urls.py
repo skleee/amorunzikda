07aae22717ef4bbfae243b1f3440cce4a828c475
namjungwon <jo_ow9@naver.com>
2019-07-30 19:20:26 +0900

    happy jw

--- hackathon/urls.py
+++ hackathon/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',scoreapp.views.home, name="home"),
     path('create/', scoreapp.views.create, name="create"),
+    path('nowgrade/', scoreapp.views.nowgrade, name="nowgrade"),
+    path('nowgrade/happy', scoreapp.views.happy, name="happy"),
+    path('nowgrade/happy/result', scoreapp.views.result, name="result"),
 ]
