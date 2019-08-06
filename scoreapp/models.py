f14ea4915df509ddf04c948a9bb8ea6c9a06978b
Seojin Kim <1106laura@naver.com>
2019-08-06 17:54:04 +0900

    model add

--- scoreapp/models.py
+++ scoreapp/models.py
@@ -6,6 +6,7 @@ class Score(models.Model):
     expected_grade = models.CharField(max_length=3)
     lecture = models.CharField(max_length=50)
     professor = models.CharField(max_length=15)
+    professor_style = models.CharField(max_length=8)
     lecture_type = models.IntegerField()
     my_score = models.FloatField()
     class_sd = models.FloatField()
@@ -13,10 +14,17 @@ class Score(models.Model):
     class_total = models.IntegerField()
     mid_ratio = models.FloatField(default=50)
     final_ratio = models.FloatField(default=50)
-
+    a_ratio = models.FloatField()
+    b_ratio = models.FloatField()
     first_grade = models.CharField(max_length=5)
     first_percentage = models.FloatField()
-    
+    happy_thinking = models.IntegerField()
+    final_percentage = models.FloatField()
+    final_grade = models.CharField(max_length=5)
+    rivals_to_win = models.IntegerField()
+    user_pw = models.CharField(max_length=20)
+    user_content = models.CharField(max_length=100)
+
     def __str__(self):
         return self.nickname
 # user_id:string
