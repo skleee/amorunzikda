6d452b3bc8b87e4ff9b482b4613f5abdcfd76923
Seojin Kim <1106laura@naver.com>
2019-08-09 19:49:28 +0900

    마이그레이션 함

--- scoreapp/models.py
+++ scoreapp/models.py
@@ -14,12 +14,12 @@ class Score(models.Model):
     final_ratio = models.FloatField(default=50)
     first_grade = models.CharField(max_length=5)
     first_percentage = models.FloatField()
-    final_percentage = models.FloatField()
     def __str__(self):
         return self.nickname
 
 class Happy(models.Model):
-    first = models.ForeignKey('Score')
+    final_percentage = models.FloatField()
+    first = models.ForeignKey('Score', on_delete=models.CASCADE)
     a_ratio2 = models.IntegerField()
     b_ratio2 = models.IntegerField()
     happythinking = models.FloatField()
@@ -27,8 +27,8 @@ class Happy(models.Model):
         return self.nickname
 
 class Submit(models.Model):
-    first = models.ForeignKey('Score')
-    second = models.Foreignkey('Happy')
+    first = models.ForeignKey('Score', on_delete=models.CASCADE)
+    second = models.ForeignKey('Happy', on_delete=models.CASCADE)
     final_grade = models.CharField(max_length=5)
     rivals_to_win = models.IntegerField()
     user_content = models.CharField(max_length=100, default=" ")
